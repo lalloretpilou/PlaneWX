f3047e2817af5ede0648b9a548b7a388b75f8282
pilou <pierrelouis95lalloret@gmail.com>
2022-08-05 10:43:38 +0200

    Initial Commit

---
+++ PlaneWX/PlaneWXApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaneWXApp.swift
+//  PlaneWX
+//
+//  Created by Pierre-Louis L'ALLORET on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlaneWXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
