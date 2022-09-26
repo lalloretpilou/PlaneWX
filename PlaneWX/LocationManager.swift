dfc6e943d1bc6d03a94eee5bd9e211b8cac91d23
pilou <pierrelouis95lalloret@gmail.com>
2022-09-26 17:46:02 +0200

    add some fix

---
+++ PlaneWX/LocationManager.swift
@@ -0,0 +1,76 @@
+//
+//  LocationManager.swift
+//  PlaneWX
+//
+//  Created by Pierre-Louis L'ALLORET on 23/09/2022.
+//
+
+import Foundation
+import CoreLocation
+
+
+class LocationManager: NSObject {
+    
+    
+    private let locationManager = CLLocationManager()
+    @Published var location: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+    }
+}
+
+
+// MARK: - Core Location Delegate
+extension LocationManager: CLLocationManagerDelegate {
+    
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didChangeAuthorization status: CLAuthorizationStatus) {
+
+        switch status {
+    
+        case .notDetermined         : print("notDetermined")        // location permission not asked for yet
+        case .authorizedWhenInUse   : print("authorizedWhenInUse")  // location authorized
+        case .authorizedAlways      : print("authorizedAlways")     // location authorized
+        case .restricted            : print("restricted")           // TODO: handle
+        case .denied                : print("denied")               // TODO: handle
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+    }
+}
+
+// MARK: - Get Placemark
+extension LocationManager {
+    
+    
+    func getPlace(for location: CLLocation,
+                  completion: @escaping (CLPlacemark?) -> Void) {
+        
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            
+            guard error == nil else {
+                print("*** Error in \(#function): \(error!.localizedDescription)")
+                completion(nil)
+                return
+            }
+            
+            guard let placemark = placemarks?[0] else {
+                print("*** Error in \(#function): placemark is nil")
+                completion(nil)
+                return
+            }
+            
+            completion(placemark)
+        }
+    }
+}
--- PlaneWX/todayView.swift
+++ PlaneWX/todayView.swift
@@ -6,17 +6,61 @@
 //
 
 import SwiftUI
+import WeatherKit
+import CoreLocation
 
 struct todayView: View {
-    @StateObject var locationViewModel = LocationViewModel()
+    
+    @State var temperature: String?
+    @State var uvIndex: UVIndex?
+    @State var symbol: String?
+    @State var status: String?
+    
+    var locationManager = LocationManager()
 
     var body: some View {
-        NavigationView {
-            Section{
-            Text("")
-            }
-            .navigationBarTitle("Today".localised())
+        
+        VStack(spacing: 10) {
+            
+            Image(systemName: symbol ?? "")
+            
+            Text("Jakarta")
+                .font(.title)
+            
+            Text(status ?? "Getting the weather")
+            
+            Text(temperature ?? "Getting the temp")
+            
+            Text("UV index " + String(uvIndex?.value ?? 0))
+            Text(uvIndex?.category.rawValue ?? "NaN")
+            
         }
+        .onAppear {
+            Task {
+                await getWeather()
+            }
+        }
+    }
+
+}
+
+
+extension todayView {
+              
+    func getWeather() async {
+        let weatherService = WeatherService()
+        let jakarta = CLLocation(latitude: locationManager.location?.latitude ?? 0
+                                 ,longitude: locationManager.location?.longitude ?? 0)
+        let weather = try? await weatherService.weather(for: jakarta)
+        
+
+        temperature=weather?.currentWeather.temperature
+            .converted(to: .celsius)
+            .formatted(.measurement(usage: .asProvided))
+        
+        uvIndex=weather?.currentWeather.uvIndex
+        symbol=weather?.currentWeather.symbolName
+        status=weather?.currentWeather.condition.rawValue
     }
 }
 
